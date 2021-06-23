80a5b7760d1b9914c7d7502157c4cdb6f3a8b7f0
sinmetal <sinmetal@sinmetalcraft.jp>
2021-06-23 15:01:28 +0900

    エラーが入ってこないので、関数を分けた

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/sinmetal/cloudrun_otel
 go 1.15
 
 require (
+	cloud.google.com/go v0.75.0
 	cloud.google.com/go/datastore v1.4.0
 	github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace v0.15.0
 	github.com/google/uuid v1.1.2
+	github.com/vvakame/sdlog v0.0.0-20200409072131-7c0d359efddc
 	go.opencensus.io v0.22.6-0.20201102222123-380f4078db9f
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.15.0
 	go.opentelemetry.io/otel v0.15.0
--- handlers.go
+++ handlers.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/vvakame/sdlog/aelog"
 	"go.opentelemetry.io/otel/label"
 	"go.opentelemetry.io/otel/trace"
 )
@@ -31,16 +32,25 @@ func (h *handlers) HelloHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func (h *handlers) Hello2Handler(w http.ResponseWriter, req *http.Request) {
+	if err := h.hello2Handler(w, req); err != nil {
+		// noop
+	}
+}
+
+func (h *handlers) hello2Handler(w http.ResponseWriter, req *http.Request) (err error) {
 	ctx := SpanContextFromHttpRequest(req.Context(), req)
 	ctx, _ = StartSpan(ctx, "hello2")
-	var err error
+	ctx = aelog.WithHTTPRequest(ctx, req)
 	defer EndSpan(ctx, err)
 
 	_, err = h.als.Insert(ctx, &AccessLog{
 		ID: uuid.New().String(),
 	})
 	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		aelog.Errorf(ctx, "failed AccessLogStore.Insert : %s", err)
 		_, _ = fmt.Fprint(w, err.Error())
+		return
 	}
 
 	msg := req.FormValue("message")
@@ -48,11 +58,14 @@ func (h *handlers) Hello2Handler(w http.ResponseWriter, req *http.Request) {
 
 	if msg == "" {
 		err = fmt.Errorf("message is required")
+		aelog.Errorf(ctx, "message is required")
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("message is required"))
-		return
+		return err
 	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(fmt.Sprintf("Ack : %s : %s", time.Now(), msg)))
+
+	return nil
 }
